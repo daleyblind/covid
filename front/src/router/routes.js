8d7712d348a8258699d9d391bf68eacb3930dcff
daleyblind <daleyblind@naver.com>
2021-06-23 23:12:45 +0900

    쓸데없는 파일 또는 라인 삭제. Map 관련 자바스크립트, vue 파일, 라우터 링크

--- front/src/router/routes.js
+++ front/src/router/routes.js
@@ -7,7 +7,6 @@ const Dashboard = () => import(/* webpackChunkName: "dashboard" */"@/pages/Dashb
 const Profile = () => import(/* webpackChunkName: "common" */ "@/pages/Profile.vue");
 const Notifications = () => import(/* webpackChunkName: "common" */"@/pages/Notifications.vue");
 const Icons = () => import(/* webpackChunkName: "common" */ "@/pages/Icons.vue");
-const Maps = () => import(/* webpackChunkName: "common" */ "@/pages/Maps.vue");
 const Typography = () => import(/* webpackChunkName: "common" */ "@/pages/Typography.vue");
 const TableList = () => import(/* webpackChunkName: "common" */ "@/pages/TableList.vue");
 
@@ -36,11 +35,6 @@ const routes = [
         path: "icons",
         name: "icons",
         component: Icons
-      },
-      {
-        path: "maps",
-        name: "maps",
-        component: Maps
       },
       {
         path: "typography",
