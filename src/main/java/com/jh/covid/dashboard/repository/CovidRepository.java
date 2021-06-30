8837a84120c05dedca61cbcf180773d06b3faed5
daleyblind <daleyblind@naver.com>
2021-06-30 15:09:58 +0900

    CovidRepository 수정 (JPA -> CRUD)

--- src/main/java/com/jh/covid/dashboard/repository/CovidRepository.java
+++ src/main/java/com/jh/covid/dashboard/repository/CovidRepository.java
@@ -1,15 +1,15 @@
 package com.jh.covid.dashboard.repository;
 
 import com.jh.covid.dashboard.domain.Covid;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 /**
  * Covid 관련 스프링 데이터 JPA.
  *
  * @since 1.0
  * @author Jang Hyun
- * @version 1.0
+ * @version 1.1
  */
 
-public interface CovidRepository extends JpaRepository<Covid, Long> {
+public interface CovidRepository extends CrudRepository<Covid, Long> {
 }
