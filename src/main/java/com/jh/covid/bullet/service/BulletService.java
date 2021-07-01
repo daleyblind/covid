88b5a8979d4524056156e81d598908e504a8d97b
daleyblind <daleyblind@naver.com>
2021-07-01 09:28:48 +0900

    건의 게시판 서비스 클래스 페이징 처리

--- src/main/java/com/jh/covid/bullet/service/BulletService.java
+++ src/main/java/com/jh/covid/bullet/service/BulletService.java
@@ -5,10 +5,11 @@ import com.jh.covid.bullet.repository.BulletQueryRepository;
 import com.jh.covid.bullet.repository.BulletRepository;
 import com.jh.covid.bullet.vo.BulletVO;
 import com.jh.covid.exception.NoDataException;
+import com.jh.covid.paging.PageDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -16,7 +17,7 @@ import java.util.Optional;
  *
  * @since 1.0
  * @author Jang Hyun
- * @version 1.0
+ * @version 1.1
  */
 
 @RequiredArgsConstructor
@@ -26,14 +27,31 @@ public class BulletService {
     private final BulletRepository repository;
     private final BulletQueryRepository queryRepository;
 
-    public List<BulletVO> findBulletList() {
-        return queryRepository.findBulletList();
+    /**
+     * 게시글 리스트를 가져오는 메서드. (페이징 처리 추가)
+     *
+     * @param pageDTO 페이징 정보를 담고 있는 객체.
+     * @return 게시글 리스트와 페이징 정보를 담고 있는 객체.
+     * @since 1.1
+     */
+    public PageDTO<BulletVO> findBulletList(PageDTO<BulletVO> pageDTO) {
+        pageDTO.setContent(queryRepository.findBulletList(pageDTO));
+        pageDTO.setTotal(repository.count());
+        return pageDTO;
     }
 
     public BulletVO findBullet(Long no) {
         return queryRepository.findBullet(no);
     }
 
+    /**
+     * 게시글 추가 메서드.
+     *
+     * @param vo 게시글 번호
+     * @return 게시글이 추가됐는지 확인하는 boolean
+     * @since 1.1
+     */
+    @Transactional
     public boolean insertBullet(BulletVO vo) {
         Bullet bullet = new Bullet();
         bullet.setTitle(vo.getTitle());
@@ -42,6 +60,14 @@ public class BulletService {
         return repository.save(bullet).getNo() > 0;
     }
 
+    /**
+     * 게시글 삭제 메서드.
+     *
+     * @param no 게시글 번호
+     * @return 게시글이 삭제됐는지 확인하는 boolean
+     * @since 1.1
+     */
+    @Transactional
     public boolean deleteBullet(Long no) {
         boolean flag = false;
         try {
@@ -53,6 +79,14 @@ public class BulletService {
         return flag;
     }
 
+    /**
+     * 게시글 수정 메서드.
+     *
+     * @param vo 건의 게시판 VO
+     * @return 게시글이 수정됐는지 확인하는 boolean
+     * @since 1.1
+     */
+    @Transactional
     public boolean modifyBullet(BulletVO vo) {
         boolean flag = false;
         Optional<Bullet> optionalBullet = repository.findById(vo.getNo());
